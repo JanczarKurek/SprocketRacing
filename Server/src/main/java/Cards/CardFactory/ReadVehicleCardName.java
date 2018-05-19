8f2e95d78892dbda9118faed02578f9fc55846e4
feliks <feliks.pobiedzinski@gmail.com>
2018-05-19 23:08:01 +0200

    Continuation of work on reading cards from file

--- Server/src/main/java/Cards/CardFactory/ReadVehicleCardName.java
+++ Server/src/main/java/Cards/CardFactory/ReadVehicleCardName.java
@@ -1,4 +1,7 @@
 package Cards.CardFactory;
 
 public class ReadVehicleCardName {
+    public static String readVehicleCardName(org.json.simple.JSONObject jsonObject) {
+        return "atok am ala";
+    }
 }
