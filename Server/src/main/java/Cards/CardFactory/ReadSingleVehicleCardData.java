1a2a02cc920d4c56494c0a84b7523d34b3ad26f2
feliks <feliks.pobiedzinski@gmail.com>
2018-05-19 23:08:01 +0200

    Continuation of work on reading cards from file

--- Server/src/main/java/Cards/CardFactory/ReadSingleVehicleCardData.java
+++ Server/src/main/java/Cards/CardFactory/ReadSingleVehicleCardData.java
@@ -2,12 +2,27 @@ package Cards.CardFactory;
 
 import org.json.simple.*;
 
+import static Cards.CardFactory.ReadVehicleCardCost.readVehicleCardCost;
+import static Cards.CardFactory.ReadVehicleCardEngine.readVehicleCardEngine;
+import static Cards.CardFactory.ReadVehicleCardId.readVehicleCardId;
+import static Cards.CardFactory.ReadVehicleCardJoints.readVehicleCardJoints;
+import static Cards.CardFactory.ReadVehicleCardName.readVehicleCardName;
+
 public class ReadSingleVehicleCardData {
     public static Cards.VehicleCardData readSingleVehicleCardData(JSONObject jsonObject) {
         Cards.VehicleCardData result = new Cards.VehicleCardData();
-        //result.se
 
+        result.setCost(readVehicleCardCost((JSONObject) jsonObject.get("Cost")));
 
-        return new Cards.VehicleCardData();
+        result.setEngine(readVehicleCardEngine((JSONObject) jsonObject.get("VehicleCardEngine")));
+
+        Boolean[] joints = readVehicleCardJoints((JSONObject) jsonObject.get("Joints"));
+        result.setJoints(joints[0], joints[1], joints[2], joints[3]);
+
+        result.setId(readVehicleCardId((JSONObject) jsonObject.get("Id")));
+
+        result.setName(readVehicleCardName((JSONObject) jsonObject.get("Name")));
+
+        return result;
     }
 }
