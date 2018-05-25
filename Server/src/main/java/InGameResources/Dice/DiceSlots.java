1b69fc2ecc53f550dbb09c278a5a25093c108a2e
JanczarKurek <janczar.kurek@student.uj.edu.pl>
2018-05-26 00:44:48 +0200

    Added proposition inner class – using of effects and checking validity now possible.

--- Server/src/main/java/InGameResources/Dice/DiceSlots.java
+++ Server/src/main/java/InGameResources/Dice/DiceSlots.java
@@ -1,6 +1,13 @@
 package InGameResources.Dice;
 
+import ErrorsAndExceptions.WrongColor;
+import ErrorsAndExceptions.WrongMove;
+
+import java.util.Collection;
+
 public interface DiceSlots extends DiceWallet{
     boolean isFull();
     int getSize();
+    void tryInsert(Dice dice) throws WrongMove, WrongColor;
+    void tryInsertAll(Collection<Dice> dice) throws WrongMove, WrongColor;
 }
