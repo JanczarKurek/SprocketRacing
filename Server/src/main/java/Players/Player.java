91bba0ff5699e47f0a71598df828079eb7168ded
JanczarKurek <janczar.kurek@student.uj.edu.pl>
2018-05-27 21:26:20 +0200

    Doing something with Player.

--- Server/src/main/java/Players/Player.java
+++ Server/src/main/java/Players/Player.java
@@ -1,10 +1,35 @@
 package Players;
 
+import Cards.Hand;
+import Cards.Layout.CardsLayout;
+import InGameResources.Dice.DiceBunch;
+import MapServer.PawnController;
+import Table.TableController;
+import Table.Table;
+
 public class Player {
     private int id;
     public int getId(){
         return id;
     }
+    private TableController tableController;
+    private Hand myHand;
+    private CardsLayout myVehicle = new CardsLayout();
+    private DiceBunch dices = new DiceBunch();
+    public Player(Table table, int id){
+        tableController = table.sitDown(this);
+        this.id = id;
+    }
 
+    public Hand getMyHand() {
+        return myHand;
+    }
 
+    public CardsLayout getMyVehicle() {
+        return myVehicle;
+    }
+
+    public DiceBunch getDices() {
+        return dices;
+    }
 }
