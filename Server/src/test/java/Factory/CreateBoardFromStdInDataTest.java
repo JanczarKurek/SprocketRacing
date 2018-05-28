caa95b66b54bbd993b6eeb61bde3be79c2f1af68
feliks <feliks.pobiedzinski@gmail.com>
2018-05-28 15:39:35 +0200

    Upgrades in Layout

--- Server/src/test/java/Factory/CreateBoardFromStdInDataTest.java
+++ Server/src/test/java/Factory/CreateBoardFromStdInDataTest.java
@@ -69,14 +69,10 @@ public class CreateBoardFromStdInDataTest {
             assertEquals(treeMap.get(8).getNextFields().size(), 1);
             assertEquals(treeMap.get(3).getNextFields().size(), 3);
 
+            assertEquals(2,((DamageEffect) treeMap.get(7).getEffects().iterator().next()).getDamage());
+            assertEquals(1,((DamageEffect) treeMap.get(3).getEffects().iterator().next()).getDamage());
 
-            for (BoardField boardField : treeMap.values())
-                System.out.println(boardField.getEffects().size());
-
-//            assertEquals(new LinkedList<>(treeMap.get(7).getEffects()).get(0).getId(), 2);
-  //          assertEquals(new LinkedList<>(treeMap.get(3).getEffects()).get(0).getId(), 1);
-
-    //        assertTrue(treeMap.get(1).getEffects().isEmpty());
+            assertTrue(treeMap.get(1).getEffects().isEmpty());
 
         } catch (NoSuchFieldException | IllegalAccessException e) {
             e.printStackTrace();
@@ -84,6 +80,51 @@ public class CreateBoardFromStdInDataTest {
 
     }
 
+
+    @org.junit.Test
+    public void test3() {
+        BoardStructure boardStructure = readBoardStructure("ExBoardStructure(2).json");
+        try {
+            java.lang.reflect.Field BSfield = BoardStructure.class.getDeclaredField("fields");
+            BSfield.setAccessible(true);
+            TreeMap<Integer, BoardField> treeMap = (TreeMap<Integer, BoardField>) BSfield.get(boardStructure);
+
+            assertTrue(isAfter(treeMap,0, 1));
+            assertTrue(isAfter(treeMap,3, 5));
+            assertTrue(isAfter(treeMap,3, 6));
+            assertTrue(isAfter(treeMap,3, 7));
+            assertTrue(isAfter(treeMap,7, 9));
+            assertTrue(isAfter(treeMap,2, 4));
+            assertTrue(isAfter(treeMap,0, 2));
+
+            assertFalse(isAfter(treeMap, 7, 4));
+            assertFalse(isAfter(treeMap, 1, 0));
+            assertFalse(isAfter(treeMap, 5, 5));
+            assertFalse(isAfter(treeMap, 7, 1));
+
+            assertEquals(treeMap.get(0).getNextFields().size(), 2);
+            assertEquals(treeMap.get(8).getNextFields().size(), 1);
+            assertEquals(treeMap.get(3).getNextFields().size(), 3);
+
+            assertEquals(2,((DamageEffect) treeMap.get(7).getEffects().iterator().next()).getDamage());
+            assertEquals(1,((DamageEffect) treeMap.get(3).getEffects().iterator().next()).getDamage());
+
+            assertTrue(treeMap.get(1).getEffects().isEmpty());
+
+            assertEquals(14, boardStructure.getEnd());
+
+            assertTrue(isAfter(treeMap, 9, 10));
+            assertTrue(isAfter(treeMap, 10, 11));
+
+            assertFalse(isAfter(treeMap, 9, 11));
+
+            assertTrue(treeMap.get(14).getNextFields().isEmpty());
+
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 
 }
