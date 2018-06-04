2ec8c06f18d865d79ed7dddaa8751a30c5b3628d
feliks <feliks.pobiedzinski@gmail.com>
2018-06-04 21:56:03 +0200

    Upgrades in reading from file

--- Server/src/main/java/Cards/CardFactory/ReadVehicleCardDataFromFile.java
+++ Server/src/main/java/Cards/CardFactory/ReadVehicleCardDataFromFile.java
@@ -5,6 +5,8 @@ import org.json.simple.*;
 import org.json.simple.parser.*;
 
 import static Cards.CardFactory.ReadSingleVehicleCardData.readSingleVehicleCardData;
+import Cards.*;
+import java.io.*;
 
 public class ReadVehicleCardDataFromFile {
     public static LinkedList<Cards.VehicleCardData> readVehicleCardDataFromFile(String fileName) {
@@ -21,5 +23,17 @@ public class ReadVehicleCardDataFromFile {
             e.printStackTrace();
         }
         return null; // zmien to potem
+    }
+
+    public static VehicleCardData readSingleVehicleCardDataFromFile(String fileName) {
+        JSONParser parser = new JSONParser();
+        try {
+            Object obj = parser.parse((new FileReader(fileName)));
+            JSONObject jsonObject = (JSONObject) obj;
+            return readSingleVehicleCardData(jsonObject);
+        } catch (java.io.IOException | ParseException e ) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
