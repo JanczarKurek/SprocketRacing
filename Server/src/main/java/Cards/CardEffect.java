46104f3c126e06b5d40f119af3c8bc88f44f6829
feliks <feliks.pobiedzinski@gmail.com>
2018-06-08 23:15:45 +0200

    Reading CardEffects from file

--- Server/src/main/java/Cards/CardEffect.java
+++ Server/src/main/java/Cards/CardEffect.java
@@ -2,11 +2,17 @@ package Cards;
 
 import misc.Effect;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class CardEffect {
     private ArrayList<ArrayList<Effect>> effects;
     public ArrayList<Effect> chooseEffect(int i){
         return effects.get(i);
+    }
+
+    public CardEffect(Collection<ArrayList<misc.Effect>> collection) {
+        effects = new java.util.ArrayList<>();
+        for (ArrayList<misc.Effect> a : collection)
+            effects.add(new java.util.ArrayList<>(a));
     }
 }
