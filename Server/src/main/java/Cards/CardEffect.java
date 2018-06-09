1f7a9c40cd04b957db9d385e43ac67b1d9be51c4
JanczarKurek <janczar.kurek@student.uj.edu.pl>
2018-06-10 01:59:53 +0200

    First serious unit-test created, and it's working after some fixes.

--- Server/src/main/java/Cards/CardEffect.java
+++ Server/src/main/java/Cards/CardEffect.java
@@ -5,7 +5,7 @@ import misc.Effect;
 import java.util.*;
 
 public class CardEffect {
-    private ArrayList<ArrayList<Effect>> effects;
+    private ArrayList<ArrayList<Effect>> effects = new ArrayList<>();
     public ArrayList<Effect> chooseEffect(int i){
         return effects.get(i);
     }
@@ -17,8 +17,10 @@ public class CardEffect {
     }
 
     public CardEffect(Effect e[][]){
-        for(Effect[] k : e){
-            effects.add(new ArrayList<>(Arrays.asList(k)));
+        for(int i = 0; i < e.length; ++i){
+            Effect[] k = e[i];
+            ArrayList<Effect> effects = new ArrayList<>(Arrays.asList(k));
+            this.effects.add(effects);
         }
     }
 }
