8b5ba9832bf583141ac96d7e8dbfc502ecfa4fc4
JanczarKurek <janczar.kurek@student.uj.edu.pl>
2018-06-12 00:47:48 +0200

    Small changes, going from list position to id.

---
+++ Server/src/main/java/Settings/JarTest.java
@@ -0,0 +1,26 @@
+package Settings;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class JarTest extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Group group = new Group();
+        Text text = new Text("Dupa biskupa!");
+        text.setTranslateY(100);
+        text.setTranslateX(100);
+        group.getChildren().add(text);
+        Stage stage = new Stage();
+        stage.setScene(new Scene(group));
+        stage.show();
+    }
+
+    public static void main(String args[]){
+        launch(args);
+    }
+}
